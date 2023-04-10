2c50589a6bf80129534f10382d6dbd29d191e133
Ivan_Zhuravlov <vanya7zhuravlov@gmail.com>
2023-04-10 16:22:05 +0300

    Module #3, task #6 completed type cast & operators presedence tasks

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,20 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
-    public void roundNumber(float numberToBeRounded) {
+    public static void main(String[] args) {
+        float number = 19.4F;
+        roundNumber(number);
+        float top = 19.5F;
+        roundNumber(top);
+    }
+    public static void roundNumber(float numberToBeRounded) {
+        int result;
+        if (numberToBeRounded < 19.49) {
+            result = (int) numberToBeRounded;}
+            else {
+            result = (int) numberToBeRounded;
+            result = ++ result;
+        }
+        System.out.println(result);
     }
 }
